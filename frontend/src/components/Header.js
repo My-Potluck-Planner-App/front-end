51f02e0727de1962627b6412e26ca968d0ef2393
Andrew Gary <andrew.gary91@gmail.com>
2022-02-28 15:53:35 -0600

    working on all the form pages

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import icon from '../images/potluck.jpg';
+import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
+
+const StyledHeader = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    .headerLinks{
+        display: flex;
+        width: 40%;
+        justify-content: space-evenly;
+        align-items: center;
+        font-size: 1.5rem;
+        margin: 0 30px;
+    }
+`
+
+
+const Header = () => {
+
+    return(
+        <StyledHeader>
+            <img src={icon} alt='Potluck Logo'></img>
+            <div className='headerLinks'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/potlucks'>Potlucks</NavLink>
+                <NavLink to='/newPotluck'>Create Potluck</NavLink>
+                <button>Logout</button>
+            </div>
+        </StyledHeader>
+    )
+}
+
+export default Header;
