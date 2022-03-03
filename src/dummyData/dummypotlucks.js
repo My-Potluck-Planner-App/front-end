618466c5da36e054aa737a89762b454bd7f0474b
Darla Young <me3u2@hotmail.com>
2022-03-03 01:24:22 -0600

    removed unneccessary extra frontend folder

---
+++ src/dummyData/dummypotlucks.js
@@ -0,0 +1,71 @@
+
+const potlucks = [
+    {
+        id: 0,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    },
+    {
+        id: 1,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    },
+    {
+        id: 2,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    },
+    {
+        id: 3,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    },
+    {
+        id: 4,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    },
+    {
+        id: 5,
+        name: 'Picnic at the park',
+        location: 'Cherry Creek Park',
+        date: '02-28-2022',
+        time: '4:00PM',
+        numberAttending: 0,
+        listOfAttendees: [],
+        itemsNeeded: [{itemToAdd: 'Cookies'}, {itemToAdd: 'Soda'},],
+        itemsClaimed: [] //These are items that someone has already agreed to bring
+    }
+]
+
+export default potlucks;
